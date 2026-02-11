9758ca1b53b341c6f90a6b3398873fee323be8ab
Jemuel Repoylo <syjem143@gmail.com>
2026-02-11 15:55:08 +0800

    fix: background components on unsupported browsers

--- src/components/backgrounds.tsx
+++ src/components/backgrounds.tsx
@@ -2,7 +2,7 @@ export const BackgroundTop = () => {
   return (
     <div
       aria-hidden="true"
-      className="absolute inset-x-0 top-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+      className="background-wrapper fixed inset-x-0 top-0 -z-10 overflow-hidden sm:-top-80"
     >
       <div className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 background-fancy sm:left-[calc(50%-30rem)] sm:w-288.75" />
     </div>
@@ -13,7 +13,7 @@ export const BackgroundBottom = () => {
   return (
     <div
       aria-hidden="true"
-      className="absolute inset-x-0 bottom-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+      className="background-wrapper fixed inset-x-0 bottom-0 -z-10 overflow-hidden sm:top-[calc(100%-30rem)]"
     >
       <div className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 background-fancy sm:left-[calc(50%+36rem)] sm:w-288.75" />
     </div>
