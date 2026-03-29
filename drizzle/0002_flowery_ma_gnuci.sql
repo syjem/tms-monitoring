fea61ef9216f69381486baaa614dc1f588432032
Jemuel Repoylo <syjem143@gmail.com>
2026-03-29 23:46:23 +0800

    migrate: add profile columns

---
+++ drizzle/0002_flowery_ma_gnuci.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "profiles" ADD COLUMN "destination" text DEFAULT 'Office';--> statement-breakpoint
+ALTER TABLE "profiles" ADD COLUMN "remarks" text DEFAULT 'Duty On Call';
---
+++ drizzle/0003_cloudy_kid_colt.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "profiles" ALTER COLUMN "destination" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "remarks" SET NOT NULL;
